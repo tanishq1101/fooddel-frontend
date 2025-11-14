70589948169deec6db04f62b0a34f5aade905c4b
Tanishq <bazball06@gmail.com>
2025-11-14 21:23:08 +0530

    frontend

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -10,7 +10,7 @@ export const StoreContextProvider = (props) => {
   const [food_list, setFoodList] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const url = "https://fooddel-backend.vercel.app/";
+  const url = "https://fooddel-backend.vercel.app";
 
   // Persist token in localStorage
   useEffect(() => {
