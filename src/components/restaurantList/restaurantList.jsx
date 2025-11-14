29c62eaf2d97c785ee68e3920aa56e08c33ede27
Tanishq <bazball06@gmail.com>
2025-11-14 18:33:51 +0530

    frontend

--- src/components/restaurantList/restaurantList.jsx
+++ src/components/restaurantList/restaurantList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./RestaurantList.css";
+import "./restaurantList.css";
 import { getUserCity } from "../../utils/getUserCity";
 import { indianCities } from "../../data/cities";
 import { StoreContext } from "../../context/StoreContext";
